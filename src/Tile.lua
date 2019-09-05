2380df08b867fba359d3f9d510a9ebcb2d95c5ab
ohohxkel <mikeecantara@gmail.com>
2019-09-05 17:53:31 +0800

    Initial commit

---
+++ src/Tile.lua
@@ -0,0 +1,50 @@
+--[[
+   CMPE40032
+    Candy Crush Clone (Match 3 Game)
+
+    -- Tile Class --
+
+
+
+    The individual tiles that make up our game board. Each Tile can have a
+    color and a variety, with the varietes adding extra points to the matches.
+]]
+
+Tile = Class{}
+
+function Tile:init(x, y, color, variety)
+    -- board positions
+    self.gridX = x
+    self.gridY = y
+
+    -- coordinate positions
+    self.x = (self.gridX - 1) * 32
+    self.y = (self.gridY - 1) * 32
+
+    -- tile appearance/points
+    self.color = color
+    self.variety = variety
+end
+
+function Tile:update(dt)
+
+end
+
+--[[
+    Function to swap this tile with another tile, tweening the two's positions.
+]]
+function Tile:swap(tile)
+
+end
+
+function Tile:render(x, y)
+    -- draw shadow
+    love.graphics.setColor(34, 32, 52, 255)
+    love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
+        self.x + x + 2, self.y + y + 2)
+
+    -- draw tile itself
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
+        self.x + x, self.y + y)
+end
